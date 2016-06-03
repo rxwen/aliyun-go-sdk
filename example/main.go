7663a73f0a59caf06a67a3124f84e50014a751c1
Raymond Wen <rx.wen218@gmail.com>
2016-06-03 14:23:42 +0800

    update example to include directmail

--- example/main.go
+++ example/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/rxwen/aliyun-go-sdk/directmail"
 	"github.com/rxwen/aliyun-go-sdk/push"
 )
 
@@ -32,4 +33,24 @@ func main() {
 	c := push.NewClient("cn-hangzhou", accessKeyID, accessKeySecret)
 	body, _ := c.SendRequest(&req)
 	fmt.Println(body)
+}
+
+func sendMail() {
+	req := directmail.MailRequest{
+		Action:         "SingleSendMail",
+		AccountName:    "notice@push.liveliy.com",
+		ReplyToAddress: true,
+		ToAddress:      "xxx@gmail.com",
+		FromAlias:      "xxx",
+		Subject:        "please activate your account",
+		HtmlBody:       "please click <a href='google.com'>here</a> to activate your account",
+	}
+
+	c := directmail.NewClient(accessKeyID, accessKeySecret)
+	body, _ := c.SendRequest(&req)
+	fmt.Println(body)
+}
+
+func main() {
+	sendMail()
 }
